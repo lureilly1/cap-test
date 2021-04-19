6f7ea200b503637537775d1669680f6840b7b933
lureilly1 <lureilly1@gmail.com>
2021-04-19 11:15:41 +0100

    con

--- src/index.js
+++ src/index.js
@@ -9,11 +9,16 @@ ReactDOM.render(
   <React.StrictMode>
     <Motor
       config={{
-          host: 'qap.sebrae.com.br',
+        host: "sense-demo.qlik.com", // Qlik Sense Host
+        secure: true, // Whether your host is secure of not (HTTPS / HTTP)
+        port: 443, // Qlik Sense site port
+        prefix: "", // Prefix
+        appId: "133dab5d-8f56-4d40-b3e0-a6b401391bde", // Application Id*/
+          /*host: 'qap.sebrae.com.br',
           secure: true,
           port: null,
           prefix: null,
-          appId: 'c66a7498-fbbf-4f81-8400-ca8f6446c47b',
+          appId: 'c66a7498-fbbf-4f81-8400-ca8f6446c47b',*/
       }}
     >
       <App />
